d131a888e296a0a14c418df9d5c7dedcb51b8369
Squashua Gonzalez <squashmvp@gmail.com>
2023-09-09 21:49:48 -0400

    Updated Titles, Trashed Reference

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('sb-btn');
-const sb = document.getElementById('sidebar');
-const sbInfo = document.getElementById('page-footer');
+sb = document.getElementById('sidebar');
+sbInfo = document.getElementById('page-footer');
+
 sb.classList.add('sidebar-visible');
 btn.innerHTML = "Show";
 
@@ -8,9 +9,9 @@ btn.addEventListener("click", function () {
   // console.log(sidebar.classList);
   if (sb.classList.contains('sidebar-visible')) {
     sb.classList.remove('sidebar-visible');
-    btn.innerHTML = "Hide"
+    btn.innerHTML = "Hide";
   } else {
     sb.classList.add('sidebar-visible');
-    btn.innerHTML = "Show"
+    btn.innerHTML = "Show";
   }
 });
